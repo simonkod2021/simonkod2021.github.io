2e5f674d9c63c13f907e548296709411eda7c3ac
simonkod2021 <simon98larsson@gmail.com>
2025-04-09 14:21:47 +0200

    fixed dropdown show content only when hover over button

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.addEventListener("")
+let currentImage = 1;
+
+function whichImage() {
+    document.querySelector("previous-picture").addEventListener("click", function (){
+        document.querySelector("currentImage" + 1)
+    })
+}
