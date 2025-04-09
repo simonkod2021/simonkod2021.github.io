fe326a44ca32c6908eeaaa84cd2372ddaa5c877d
simonkod2021 <simon98larsson@gmail.com>
2025-04-09 17:22:37 +0200

    added collapsible button

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let currentImage = 1;
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-function whichImage() {
-    document.querySelector("previous-picture").addEventListener("click", function (){
-        document.querySelector("currentImage" + 1)
-    })
-}
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
